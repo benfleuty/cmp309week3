e9e6159cbfb82a35cf7e3b0c56cd0a00bc12db24
Ben Fleuty <bfleuty@outlook.com>
2022-03-04 06:37:09 +0000

    Add click handler for buttons with dummy toasts

--- IntentsandActivities/app/src/main/java/uk/ac/abertay/intentsandactivities/MainActivity.java
+++ IntentsandActivities/app/src/main/java/uk/ac/abertay/intentsandactivities/MainActivity.java
@@ -1,17 +1,36 @@
 package uk.ac.abertay.intentsandactivities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    /** Gets the selected contact's phone number and put it into the EditText field */
-    private void getContactNumber(Intent intent){
+
+    Toast lastToast;
+
+    public void Toaster(String text) {
+        Toaster(text, true);
+    }
+
+    public void Toaster(String text, boolean cancel) {
+        Toast nextToast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        if (cancel && lastToast != null) {
+            lastToast.cancel();
+        }
+        nextToast.show();
+        lastToast = nextToast;
+    }
+
+    /**
+     * Gets the selected contact's phone number and put it into the EditText field
+     */
+    private void getContactNumber(Intent intent) {
         // 1. Get data from Intent
         Uri contactUri = intent.getData();
         // 2. Create a search filter for all values we need by putting their types into an array.
@@ -27,8 +46,26 @@ public class MainActivity extends AppCompatActivity {
             // no need to loop through the entire table, just get the FIRST row.
             int numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER); // find out what the index for the phone number column is
             String number = cursor.getString(numberIndex); // use this index to get the string value from the column in the current row
-            /** Don't forget to change the "inputFieldID" to the id of your EditText field */
+            /* Don't forget to change the "inputFieldID" to the id of your EditText field */
 //            ((EditText)findViewById(R.id.inputFieldID)).setText(number); // set the text of the input field to the phone number string
+        }
+    }
+
+    public void handleClick(View view) {
+        int action = view.getId();
+        int btnDialNumberId = findViewById(R.id.btnDialNumber).getId();
+        int btnGetContentId = findViewById(R.id.btnGetContact).getId();
+        int btnGoToWebId = findViewById(R.id.btnGoToWeb).getId();
+
+
+        if (action == btnDialNumberId) {
+            Toaster("dial number clicked");
+        } else if (action == btnGetContentId) {
+            Toaster("get contact clicked");
+        } else if (action == btnGoToWebId) {
+            Toaster("go to web clicked");
+        } else {
+            Toaster("error");
         }
     }
 
