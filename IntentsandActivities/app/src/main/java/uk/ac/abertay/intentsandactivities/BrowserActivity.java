dfd2f6d65bcf4760a6ff3d9c53965beeb92370d3
Ben Fleuty <bfleuty@outlook.com>
2022-03-04 09:26:44 +0000

    Open web links from external sources

--- IntentsandActivities/app/src/main/java/uk/ac/abertay/intentsandactivities/BrowserActivity.java
+++ IntentsandActivities/app/src/main/java/uk/ac/abertay/intentsandactivities/BrowserActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class BrowserActivity extends AppCompatActivity {
 
@@ -12,8 +15,14 @@ public class BrowserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
-
-        loadWebView(this.getIntent().getStringExtra("webAddressToLoad"));
+        String uri = this.getIntent().getStringExtra("webAddressToLoad");
+        if(uri == null) uri = this.getIntent().getDataString();
+        if(uri == null){
+            Toast.makeText(this, "There is an issue with the provided link and the browser cannot open!", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_main);
+            return;
+        }
+        loadWebView(uri);
     }
 
     private void loadWebView(String uri) {
